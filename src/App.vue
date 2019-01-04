1b94b576ef80d6b2450f4f113f17c6cd78eec28c
jing123442 <3446484442@qq.com>
2019-01-04 21:01:06 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -7,60 +7,6 @@
   </div>
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
-        content: '',
-        history: []
-      }
-    },
-    methods: {
-      clickToAbout() {},
-      clickToHome() {}
-    },
-    mounted() {
-
-
-
-
-
-      var obj = function() {
-        this.eventsArr = {
-        };
-        this.click = function() {
-          this.eventsArr['click'].forEach(item => {
-            item();
-          })
-        };
-        this.addEventListener = function(name, callback) {
-          if (this.eventsArr[name]) {
-            this.eventsArr[name].push(callback)
-          } else {
-            this.eventsArr[name] = [callback];
-          }
-        };
-      }
-      var per1 = new obj();
-      var per2 = new obj();
-      per1.addEventListener('click', function() {
-        console.log('object2');
-      });
-      per1.addEventListener('click', function() {
-        console.log('object1');
-      });
-       per1.addEventListener('click', function() {
-        console.log('object3');
-      });
-      per1.click();
-      console.log('>>>>>>>>>>>>');
-       per2.addEventListener('click', function() {
-        console.log('ppppppp');
-      });
-      per2.click();
-    }
-  };
-</script>
 
 <style lang="scss">
   #app {
